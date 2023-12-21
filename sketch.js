8414986b8d24b39f23b427752555aaf55119dd38
Monica <monicahutama@gmail.com>
2023-12-21 19:41:32 +0700

    modified the JS and CSS to create num X num grid and to be able to click and change the colour of each square rather than just hovering over. Additionally, the colors are randomized.

--- sketch.js
+++ sketch.js
@@ -3,11 +3,7 @@ window.addEventListener("load", squares);
 function squares() {
     const container = document.querySelector("#container");
     container.innerHTML = '';
-    for(i=1; i<= 16; i++){
-        const square = document.createElement("div");
-        square.className = 'square';
-        container.appendChild(square);
-    }
+    createSquares(16);
 }
 
 const generate = document.querySelector(".generate");
@@ -17,10 +13,40 @@ generate.addEventListener('click', generateNew);
 function generateNew() {
     let squareNumber = parseInt(prompt('Number of squares: '));
 
-    container.innerHTML = '';
-    for(i=1; i <= squareNumber; i++) {
-        const square = document.createElement("div");
-        square.className = 'square';
-        container.appendChild(square);
+    if(squareNumber <= 100) {
+        container.innerHTML = '';
+        createSquares(squareNumber);
+    } else if(squareNumber > 100) {
+        alert('Number is too big! Cannot depass 100.');
     }
+}
+
+function createSquares(squareNumber) {
+    const container = document.querySelector("#container");
+    const containerSize = 480;
+    const squareSize = containerSize / squareNumber;
+
+    for(let i = 1; i <= squareNumber; i++) {
+        for(let j = 1; j <= squareNumber; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            square.style.width = squareSize + 'px';
+            square.style.height = squareSize + 'px';
+
+            square.addEventListener('click', changeColor);
+
+            container.appendChild(square);
+        }
+    }
+}
+
+function changeColor(event) {
+    const square = event.target;
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const randomColor = `rgb(${red}, ${green}, ${blue})`;
+
+    square.style.backgroundColor = randomColor;
 }
